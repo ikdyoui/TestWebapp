6e760a8527ab192c2d1ed0ba14018527babfbba8
Yuichi Ikeda <ikdyoui@gmail.com>
2014-10-29 18:49:49 +0900

    hoge

--- config/production.pl
+++ config/production.pl
@@ -14,7 +14,6 @@ my $dbname = substr($uri->path, 1);
 my ($user_pass, $host_port) = split /@/, $uri->authority;
 my ($user, $pass) = split /:/, $user_pass;
 my ($host, $port) = split /:/, $host_port;
-my ($user2, $pass2) = split /:/, $uri->userinfo;
 
 +{
     'DBI' => [
