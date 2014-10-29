f89a1dcc35037d3ee2d2ed7902497685900feee6
Yuichi Ikeda <ikdyoui@gmail.com>
2014-10-29 17:35:58 +0900

    mod config

--- config/production.pl
+++ config/production.pl
@@ -15,7 +15,6 @@ my $host = $uri->host;
 my $port = $uri->port;
 my ($user, $pass) = split /:/, $uri->userinfo;
 
-print "prod\n";
 +{
     'DBI' => [
         "dbi:Pg:dbname=$dbname;host=$host;port=$port", $user, $pass,
