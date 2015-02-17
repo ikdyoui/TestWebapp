b10b7a1b86b9b0fb4c874709806f66997dfd6038
Yuichi Ikeda <ikdyoui@gmail.com>
2015-02-17 19:20:50 +0900

    mod pg init

--- sql/pg-init.sh
+++ sql/pg-init.sh
@@ -1,2 +1,3 @@
-sudo su - postgres -c 'createdb testwebapp_dev'
+# sudo su - postgres -c 'createdb testwebapp_dev'
+sudo su - postgres -c 'createdb --echo --port=5432 --host=localhost --locale=ja_JP.UTF8 --encoding=UTF8 testwebapp_dev'
 sudo su - postgres -c 'psql -f '$PWD'/sql/pg.sql testwebapp_dev'
